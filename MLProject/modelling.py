919061a6538081905dcec9920fe76127fc1d0b58
fikriars <tbfikri.arsyad@gmail.com>
2026-07-24 04:06:27 +0700

    Initial commit for Kriteria 3

---
+++ MLProject/modelling.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("Latihan_Basic_Model_CI")
+
+df = pd.read_csv('data_clean.csv')
+
+X = df.drop(columns=['blueWins'])
+y = df['blueWins']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+mlflow.autolog()
+
+with mlflow.start_run():
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    
+    print(f"CI Basic Model Accuracy: {accuracy:.4f}")
